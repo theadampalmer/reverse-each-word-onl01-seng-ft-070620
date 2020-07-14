733f9215fa67e558c71bb15bef137e9d4f3b4b48
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 00:21:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,20 @@
 # "Hi again, just making sure it's reversed!"
 
 # end
+
+require "pry"
+
+# #def reverse_each_word(sentence)
+# #binding.pry
+# new_array_sentence = []
+# sentence.split(" ").each do |word|
+# new_array_sentence << word.reverse
+# end
+# new_array_sentence.join(" ")
+# # end
+
+def reverse_each_word(sentence)
+#binding.pry
+
+sentence.split(" ").collect { |word| word.reverse }.join(" ")
+end
