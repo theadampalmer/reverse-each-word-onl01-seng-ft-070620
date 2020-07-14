d1884e866c6b1098c51c56d75a424d38494d3550
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 00:22:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,26 +1,13 @@
-
-
-# def reverse_each_word(sentence 1)
-  
-#   "Hello there, and how are you?"
-  
-# end
-
-# def reverse_each_word(sentence 2)
-# "Hi again, just making sure it's reversed!"
-
-# end
-
 require "pry"
 
-# #def reverse_each_word(sentence)
-# #binding.pry
-# new_array_sentence = []
-# sentence.split(" ").each do |word|
-# new_array_sentence << word.reverse
+#def reverse_each_word(sentence)
+#binding.pry
+new_array_sentence = []
+sentence.split(" ").each do |word|
+new_array_sentence << word.reverse
+end
+new_array_sentence.join(" ")
 # end
-# new_array_sentence.join(" ")
-# # end
 
 def reverse_each_word(sentence)
 #binding.pry
